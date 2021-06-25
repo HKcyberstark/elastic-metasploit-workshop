10f32adbe62af25d7a880b4176fde17dff4cada2
Haran Kumar <40884455+HKcyberstark@users.noreply.github.com>
2021-06-25 11:16:56 +0800

    Update systemsetup.ps1

--- systemsetup.ps1
+++ systemsetup.ps1
@@ -1,6 +1,7 @@
 $SrcUri = "https://raw.githubusercontent.com/HKcyberstark/elastic-metasploit-workshop/master/"
 $WinUri = "https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-7.13.2-windows-x86_64.zip"
 $NoteplusUri = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.1/npp.8.1.Installer.x64.exe"
+$apt = "https://raw.githubusercontent.com/HKcyberstark/elastic-metasploit-workshop/master/apt34.ps1"
 $HomeDir = "C:\Users\Administrator\Elastic\"
 $Command = "Set-MpPreference -DisableRealtimeMonitoring $true"
 
@@ -11,5 +12,5 @@ Invoke-WebRequest -Uri "$NoteplusUri" -OutFile "$HomeDir\notepadplus.exe
 Invoke-WebRequest -Uri "$WinUri" -OutFile "$HomeDir\winlogbeat.zip"
 Expand-Archive -Path $HomeDir\winlogbeat.zip -DestinationPath $HomeDir\winlogbeat
 Invoke-WebRequest -Uri "$SrcUri/sysmon-install.ps1" -OutFile "$HomeDir\sysmon-install.ps1"
-Invoke-WebRequest -Uri "$SrcUri/apt34.ps1" -OutFile "$HomeDir\apt34.ps1"
+Invoke-WebRequest -Uri "$apt" -OutFile "$HomeDir\apt34.ps1"
 Invoke-Expression $Command
