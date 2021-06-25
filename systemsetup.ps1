a2abbb8565e964978beecca6bdaf3c8512d5107d
Haran Kumar <40884455+HKcyberstark@users.noreply.github.com>
2021-06-25 10:43:55 +0800

    Create systemsetup.ps1

---
+++ systemsetup.ps1
@@ -0,0 +1,16 @@
+$SrcUri = "https://raw.githubusercontent.com/HKcyberstark/elastic-metasploit-workshop/master/"
+$WinUri = "https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-7.13.2-windows-x86_64.zip"
+$NoteplusUri = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.1/npp.8.1.Installer.x64.exe"
+$HomeDir = "C:\Users\Administrator\Elastic\"
+New-Item -Path $HomeDir -Type directory
+$Command = "Set-MpPreference -DisableRealtimeMonitoring $true"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+Invoke-WebRequest -Uri "$NoteplusUri" -OutFile "$HomeDir\notepadplus.exe
+Invoke-WebRequest -Uri "$WinUri" -OutFile "$HomeDir\winlogbeat.zip"
+Expand-Archive -Path $HomeDir\winlogbeat.zip -DestinationPath $HomeDir\winlogbeat
+Invoke-WebRequest -Uri "$SrcUri/sysmon-install.ps1" -OutFile "$HomeDir\sysmon-install.ps1"
+Invoke-WebRequest -Uri "$SrcUri/apt34.ps1" -OutFile "$HomeDir\apt34.ps1"
+Invoke-Expression $Command
